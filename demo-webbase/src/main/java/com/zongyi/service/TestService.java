d4bbe2877c262fe5279bd2b98d768b6c805f0b16
冯宗毅 <fzy657300@163.com>
2019-04-22 10:42:29 +0800

    .

--- demo-webbase/src/main/java/com/zongyi/service/TestService.java
+++ demo-webbase/src/main/java/com/zongyi/service/TestService.java
@@ -1,10 +1,12 @@
 package com.zongyi.service;
 
+import com.zongyi.service.impl.TestServiceImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("demo-ucenter")
+@FeignClient(name = "demo-ucenter", fallback = TestServiceImpl.class)
+
 public interface TestService {
     @RequestMapping("/ucenterTestController/testMethod")
     String test(@RequestParam("name") String name);
